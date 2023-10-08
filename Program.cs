d710d4df4d51709ecda73211a87f2ef67f75f9ea
AleksanderOkrasa <95458102+AleksanderOkrasa@users.noreply.github.com>
2023-10-08 14:49:04 +0200

    sorting out the code

--- Program.cs
+++ Program.cs
@@ -31,6 +31,19 @@ namespace SchoolApp
                 ShowCourses();
                 ShowCourseDetail();
 
+
+                void AddTeacher()
+                {
+                    Console.WriteLine("Podaj imię i nazwisko nauczyciela:");
+                    string teacherName = Console.ReadLine();
+                    Console.WriteLine("Podaj nazwę przedmiotu (prowadzonego przez powyższego nauczyciela):");
+                    string teacherSubject = Console.ReadLine();
+                    var newTeacher = new Teacher { Name = teacherName, Subject = teacherSubject };
+                    db.Teachers.Add(newTeacher);
+                    db.SaveChanges();
+                }
+
+
                 void AddCourse()
                 {
                     Console.WriteLine("Podaj nazwę kursu:");
@@ -45,7 +58,66 @@ namespace SchoolApp
                     db.Courses.Add(newCourse);
                     db.SaveChanges();
                 }
-                
+
+
+                void AddStudent()
+                {
+                    Console.WriteLine("Podaj imię i nazwisko nowego studenta:");
+                    string studentName = Console.ReadLine();
+                    var newStudent = new Student { Name = studentName };
+                    db.Students.Add(newStudent);
+                    db.SaveChanges();
+                }
+
+                void ShowStudents()
+                {
+                    Console.WriteLine("Studenci:");
+                    var students = db.Students.ToList();
+                    foreach (var student in students)
+                    {
+                        Console.WriteLine(student.StudentId + ". " + student.Name);
+                    }
+                }
+
+                void UpdateStudent()
+                {
+                    ShowStudents();
+                    Console.WriteLine("Podaj ID studenta do aktualizacji:");
+                    int studentIdToUpdate = Convert.ToInt32(Console.ReadLine());
+                    var studentToUpdate = db.Students.Find(studentIdToUpdate);
+                    if (studentToUpdate != null)
+                    {
+                        Console.WriteLine("Podaj nowe imię i nazwisko:");
+                        studentToUpdate.Name = Console.ReadLine();
+                        db.SaveChanges();
+                    }
+                }
+
+                void DeleteStudent()
+                {
+                    ShowStudents();
+                    Console.WriteLine("Podaj ID studenta do usunięcia:");
+                    int studentIdToDelete = Convert.ToInt32(Console.ReadLine());
+                    var studentToDelete = db.Students.Find(studentIdToDelete);
+                    if (studentToDelete != null)
+                    {
+                        db.Students.Remove(studentToDelete);
+                        db.SaveChanges();
+                    }
+                }
+
+
+                void ShowTeachers()
+                {
+                    Console.WriteLine("Nauczyciele:");
+                    var teachers = db.Teachers.ToList();
+                    foreach (var teacher in teachers)
+                    {
+                        Console.WriteLine(teacher.TeacherId + ". " + teacher.Name + " -> " + teacher.Subject);;
+                    }
+                }
+
+
                 void ShowCourses()
                 {
                     Console.WriteLine("Kursy:");
@@ -58,9 +130,10 @@ namespace SchoolApp
 
                 void ShowCourseDetail()
                 {
+                    ShowCourses();
                     Console.WriteLine("Podaj ID kursu do wyświetlenia szczegółów:");
                     int courseIdToFind = Convert.ToInt32(Console.ReadLine());
-                    var courseToShowDetail= db.Courses.Find(courseIdToFind);
+                    var courseToShowDetail = db.Courses.Find(courseIdToFind);
                     if (courseToShowDetail != null)
                     {
                         Console.WriteLine("Nazwa: " + courseToShowDetail.Name);
@@ -70,7 +143,7 @@ namespace SchoolApp
                         {
                             Console.WriteLine(student.StudentId + ". " + student.Name);
                         }
-                            db.SaveChanges();
+                        db.SaveChanges();
                     }
                 }
 
@@ -95,7 +168,7 @@ namespace SchoolApp
                         {
                             int teacherId = Convert.ToInt32(Console.ReadLine());
                             var teacherToUpdate = db.Teachers.Find(teacherId);
-                            
+
                             if (teacherToUpdate != null)
                             {
                                 courseToUpdate.Teacher = teacherToUpdate;
@@ -120,71 +193,6 @@ namespace SchoolApp
                     }
                 }
 
-
-
-                void AddTeacher()
-                {
-                    Console.WriteLine("Podaj imię i nazwisko nauczyciela:");
-                    string teacherName = Console.ReadLine();
-                    Console.WriteLine("Podaj nazwę przedmiotu (prowadzonego przez powyższego nauczyciela):");
-                    string teacherSubject = Console.ReadLine();
-                    var newTeacher = new Teacher { Name = teacherName, Subject = teacherSubject };
-                    db.Teachers.Add(newTeacher);
-                    db.SaveChanges();
-                }
-
-                void AddStudent()
-                {
-                    Console.WriteLine("Podaj imię i nazwisko nowego studenta:");
-                    string studentName = Console.ReadLine();
-                    var newStudent = new Student { Name = studentName };
-                    db.Students.Add(newStudent);
-                    db.SaveChanges();
-                }
-
-                void ShowStudents()
-                {
-                    // Odczytywanie i wyświetlanie wszystkich studentów
-                    Console.WriteLine("Studenci:");
-                    var students = db.Students.ToList();
-                    foreach (var student in students)
-                    {
-                        Console.WriteLine(student.StudentId + ". " + student.Name);
-                    }
-
-                }
-                void ShowTeachers()
-                {
-                    Console.WriteLine("Nauczyciele:");
-                    var teachers = db.Teachers.ToList();
-                    foreach (var teacher in teachers)
-                    {
-                        Console.WriteLine(teacher.TeacherId + ". " + teacher.Name + " -> " + teacher.Subject);;
-                    }
-                }
-                void DeleteStudent()
-                {
-                    Console.WriteLine("Podaj ID studenta do usunięcia:");
-                    int studentIdToDelete = Convert.ToInt32(Console.ReadLine());
-                    var studentToDelete = db.Students.Find(studentIdToDelete);
-                    if (studentToDelete != null)
-                    {
-                        db.Students.Remove(studentToDelete);
-                        db.SaveChanges();
-                    }
-                }
-                void UpdateStudent()
-                {
-                    Console.WriteLine("Podaj ID studenta do aktualizacji:");
-                    int studentIdToUpdate = Convert.ToInt32(Console.ReadLine());
-                    var studentToUpdate = db.Students.Find(studentIdToUpdate);
-                    if (studentToUpdate != null)
-                    {
-                        Console.WriteLine("Podaj nowe imię i nazwisko:");
-                        studentToUpdate.Name = Console.ReadLine();
-                        db.SaveChanges();
-                    }
-                }
 
             }
         }
